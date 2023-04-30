50cf4a1afa0e39bab86433118ead4d0d2bc78f11
ShinjungOh <tlswjd9260@gmail.com>
2023-04-30 22:16:59 +0900

    feat: Reset, GlobalStyles 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Reset } from 'styled-reset';
+import GlobalStyle from './styles/GlobalStyle';
+
 export default function App() {
   return (
-    <h1>메가테라 푸드코트 키오스크</h1>
+    <>
+      <Reset />
+      <GlobalStyle />
+      <div>
+        <h1>메가테라 푸드코트 키오스크</h1>
+      </div>
+    </>
   );
 }

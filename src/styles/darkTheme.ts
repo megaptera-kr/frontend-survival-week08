fb6cc0ecb7c60af94ba7e121557c4044593bffd7
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2023-12-06 23:59:51 +0900

    add: Theme 설정

---
+++ src/styles/darkTheme.ts
@@ -0,0 +1,20 @@
+import Theme from './Theme';
+
+const darkTheme:Theme = {
+  colors: {
+    primary: '#1E1E1E',
+    secondary: '#442728',
+    tertiary: '#1E1E1E',
+    buttonText: '#FFF',
+    buttonHover: '',
+    buttonHoverBorder: '3px solid #FF8400',
+    searchBorder: '2px solid #FF8400',
+    background: '#3A3A3A',
+    layoutBackground: '#1E1E1E',
+    text: '#FFF',
+    activeBtn: '#D87000',
+    activeCancelBtn: '#170A0C',
+  },
+};
+
+export default darkTheme;
---
+++ src/styles/defaultTheme.ts
@@ -0,0 +1,18 @@
+const defaultTheme = {
+  colors: {
+    primary: '#FF8400',
+    secondary: '#442728',
+    tertiary: '#F4F4F4',
+    buttonText: '#000',
+    buttonHover: '#FFF1DC',
+    buttonHoverBorder: '',
+    searchBorder: '2px solid #FFF',
+    background: '#FFF',
+    text: '#FFF',
+    layoutBackground: 'linear-gradient(134deg, #F89E21 0.7%, #FF6400 65.66%)',
+    activeBtn: '#D87000',
+    activeCancelBtn: '#170A0C',
+  },
+};
+
+export default defaultTheme;

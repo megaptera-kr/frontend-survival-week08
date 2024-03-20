f1571360f567b8b193ecca03834321afc66309e6
swd <tladnjsen@gmail.com>
2024-03-20 11:02:50 +0900

    chore: Add some chore works
    - npm i react-router-dom
    - npm i -D eslint-config-prettier
    - add .node-version (v20.11.0)

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 
 import App from './App';
 
@@ -9,7 +10,11 @@ function main() {
   }
 
   const root = ReactDOM.createRoot(container);
-  root.render(<App />);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
 }
 
 main();

6c0248ee9fe54577053c3e21b045565e62aa4474
swd <tladnjsen@gmail.com>
2024-03-20 12:50:17 +0900

    feat: Add GlobalStyle.ts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,8 @@
-import { Reset } from 'styled-reset';
 import { Outlet } from 'react-router';
+
+import { Reset } from 'styled-reset';
+
+import GlobalStyle from '../styles/GlobalStyle';
 
 import Header from './Header';
 import Footer from './Footer';
@@ -8,6 +11,7 @@ export default function Layout() {
   return (
     <>
       <Reset />
+      <GlobalStyle />
       <Header />
       <main>
         <Outlet />

8d830a239f5b3a22f0aab1aab14e23f83da62480
WATA-Yiju <yiju.dev@watanow.com>
2024-05-09 15:19:39 +0900

    add:order 타입 및 mock 데이터 추가

--- fixtures/index.ts
+++ fixtures/index.ts
@@ -1,6 +1,10 @@
 import cart from './cart';
+import food from './food';
+import order from './order';
 import restaurants from './restaurants';
 
-const fixtures = { restaurants, cart };
+const fixtures = {
+  restaurants, cart, food, order,
+};
 
 export default fixtures;

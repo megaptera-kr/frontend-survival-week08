d77e3334190be40dae023af7778cd6937f63457d
WATA-Yiju <yiju.dev@watanow.com>
2024-05-09 21:58:42 +0900

    style(Header): 스타일링

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,35 @@
+import styled from 'styled-components';
+import DarkModeToggle from './DarkModeToggle';
+
+const StyledHeader = styled.header`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 50px 30px;
+`;
+
+const StyledHeaderLeft = styled.div`
+  display:flex;
+  align-items: center;
+  column-gap:20px;
+`;
+
+const StyledHeaderTitle = styled.h1`
+  font-family: ${(props) => props.theme.fontFamily.Jalnan};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
+  font-size:${(props) => props.theme.fontSize.xl};
+  color:${(props) => props.theme.colors.title};
+`;
+
 function Header() {
   return (
-    <header>
-      <img src="logo" alt="logo" />
-      <h1>메가테라 푸드코트 키오스크</h1>
-      <button type="button">밝게/어둡게</button>
-    </header>
+    <StyledHeader>
+      <StyledHeaderLeft>
+        <img src="images/logo.png" alt="logo" width={80} />
+        <StyledHeaderTitle>메가테라 푸드코트 키오스크</StyledHeaderTitle>
+      </StyledHeaderLeft>
+      <DarkModeToggle />
+    </StyledHeader>
   );
 }
 

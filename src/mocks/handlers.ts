7a3471949c83887e9b902809f268c562c4ff916e
WATA-Yiju <yiju.dev@watanow.com>
2024-05-09 16:43:34 +0900

    feat:useFetchOrder, usePostOrder 테스트 코드 추가 및 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,12 +3,16 @@ import { rest } from 'msw';
 import fixtures from '../../fixtures';
 import BASE_URL from '../api';
 
-const { restaurants } = fixtures;
+const { restaurants, order } = fixtures;
 
 const handlers = [
   rest.get(`${BASE_URL}/restaurants`, (req, res, ctx) => res(
     ctx.status(200),
     ctx.json({ restaurants }),
+  )),
+  rest.get(`${BASE_URL}/orders/:id`, (req, res, ctx) => res(
+    ctx.status(200),
+    ctx.json({ order }),
   )),
 ];
 
